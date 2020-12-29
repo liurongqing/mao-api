1a9d3e9d6f57c1464d8d63af0ec0b71a12821ae7
liurongqing <phpgege@gmail.com>
2020-12-29 12:48:04 +0800

    跨域处理201

--- src/app.ts
+++ src/app.ts
@@ -15,17 +15,18 @@ const app = new Koa();
 db();
 
 // 跨域设置
-app.use(cors({
-  origin: function (ctx: any) {
-    const { origin } = ctx.request.header;
-    if (['http://localhost:4200', 'http://127.0.0.1:4200', 'https://manage.henmao.com'].includes(origin)) {
-      return origin;
-    }
-    return false;
-  },
-  allowHeaders: ['Authorization'],
-  exposeHeaders: ['Authorization']
-}));
+// app.use(cors({
+//   origin: function (ctx: any) {
+//     const { origin } = ctx.request.header;
+//     if (['http://localhost:4200', 'http://127.0.0.1:4200', 'https://manage.henmao.com'].includes(origin)) {
+//       return origin;
+//     }
+//     return false;
+//   },
+//   allowHeaders: ['Authorization'],
+//   exposeHeaders: ['Authorization']
+// }));
+
 
 // 无授权处理, 未登录，或过期
 app.use(function (ctx: any, next: any) {
