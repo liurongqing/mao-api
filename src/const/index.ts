27ffc175810a2cee6b33400921aba5883e209314
liurongqing <phpgege@gmail.com>
2021-01-22 17:20:30 +0800

    3

--- src/const/index.ts
+++ src/const/index.ts
@@ -2,8 +2,8 @@ export const WX_BASE_PATH = 'https://api.weixin.qq.com';
 export const APP_ID = 'wx6ef1e85ccdd6b748';
 export const APP_SECRET = '7303c7f9b4fe3cfc3768b2f5c7d4c435';
 
-export const LIFE = 5;
-export const SHARE_NUM = 5;
+export const LIFE = 3;
+export const SHARE_NUM = 3;
 
 /**
  * 错误码
