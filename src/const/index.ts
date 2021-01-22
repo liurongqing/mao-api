5a976077c5b0383f1e5d549da9d009ccaabea0e7
liurongqing <phpgege@gmail.com>
2021-01-22 16:38:25 +0800

    1

--- src/const/index.ts
+++ src/const/index.ts
@@ -2,7 +2,7 @@ export const WX_BASE_PATH = 'https://api.weixin.qq.com';
 export const APP_ID = 'wx6ef1e85ccdd6b748';
 export const APP_SECRET = '7303c7f9b4fe3cfc3768b2f5c7d4c435';
 
-export const LIFE = 100;
+export const LIFE = 5;
 export const SHARE_NUM = 5;
 
 
